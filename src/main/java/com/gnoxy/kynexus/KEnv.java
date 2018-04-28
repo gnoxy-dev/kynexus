7312d167d365b1d76841eed8d2a5f77b6088e8de
gnoxy <dev@gnoxy.com>
2018-04-28 16:14:59 -0500

    Added DeploymentConfig that points to DockerHub.

--- src/main/java/com/gnoxy/kynexus/KEnv.java
+++ src/main/java/com/gnoxy/kynexus/KEnv.java
@@ -7,11 +7,9 @@ package com.gnoxy.kynexus;
 
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -24,7 +22,6 @@ public class KEnv {
 
     private static KEnv instance;
 
-//    public static void main(String[] args) throws IOException, XmlPullParserException {
     private KEnv() {
         MavenXpp3Reader reader = new MavenXpp3Reader();
         Model model;
